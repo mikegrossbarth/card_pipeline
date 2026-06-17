7442c3ed058aefcc63b61cb6c5fa97a86b03b268
Mike Grossbarth <mikegrossbarth@gmail.com>
2026-06-17 10:03:17 -0500

    Target full Card Ladder grader bar

--- comp_engine/bridge_server.py
+++ comp_engine/bridge_server.py
@@ -16,8 +16,8 @@ from urllib.parse import parse_qs, urlparse
 from cardladder_ocr import extract_cl_value_from_data_url
 from workbook_io import WorkbookRow
 
-BRIDGE_VERSION = "2026-06-17-cardladder-simple-grader-flow-v12"
-EXPECTED_CARDLADDER_EXTENSION_VERSION = "2026-06-17-simple-grader-flow-v12"
+BRIDGE_VERSION = "2026-06-17-cardladder-direct-grader-set-v13"
+EXPECTED_CARDLADDER_EXTENSION_VERSION = "2026-06-17-direct-grader-set-v13"
 EXPECTED_CARDLADDER_MANIFEST_VERSION = "0.1.5"
 DEBUG_DIR = Path(__file__).resolve().parent.parent / "work" / "cardladder-bridge"
 COMP_STRATEGY_AVERAGE = "average_last_5"
